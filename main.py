525d5a532cbced838c4dd475b4150ddb2390474b
danny-smirnov <danu90_2016@mail.ru>
2023-05-11 15:43:38 +0300

    some stuff

--- main.py
+++ main.py
@@ -1,1 +1,59 @@
-print('hello git')
+# Necessary libraries
+from utils import solve, InputData, animate, plot_h0
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == '__main__':
+    k = 176.1  # permeability coef
+    drho = 814.4  # oil-gas pressure difference
+    g = 9.80665  # acceleration of gravity
+    mu = 18.76  # dynamic viscosity
+    phi = 0.266  # effective porosity
+
+    h_0 = 7  # initial height
+    W = 150  # half of the drain length
+    L = 1171.3  # well length
+    dw = 0.089  # well diameter
+    zw1 = 5.6
+    zw2 = zw1 - dw
+    cube_to_kg = 800  # coefficient to translate m^3 to kg
+    p1 = 30  # pressure at t1 point
+    p2 = 10  # pressure at t3 point
+    Bg = 0.00935
+    Bo = 1.069
+    gamma = 100
+    beta = 0.191794288875527
+
+    p0 = drho * g * h_0 / 10 ** 5  # bar
+    q0 = 2 * k * p0 * L * h_0 / mu / W / 115.74
+    t0 = 115.74 * W ** 2 * mu * phi / k / p0
+
+    inp = InputData(path='./data/1501.dat',
+                    cube_to_kg=cube_to_kg, k=k,
+                    W=W, L=150,
+                    g=g, mu=mu,
+                    phi=phi, h_0=h_0,
+                    zw1=zw1, zw2=zw2,
+                    dw=dw, n_t=100,
+                    n_x=100, drho=drho,
+                    Bo=Bo, Bg=Bg,
+                    beta=beta, gamma=gamma,
+                    p1=p1, p2=p2,
+                    p0=p0, q0=q0, t0=t0)
+
+    num_of_t = [100, 500, 1000]
+    num_of_x = [100, 500, 1000]
+    sol_overall = []
+    for n_x in num_of_x:
+        inp.n_x = n_x
+        sol = solve(inp)
+        # plot_h0(sol)
+        sol_overall.append(sol[:, 0])
+
+    # plt.legend()
+    # plt.show()
+    plt.plot(np.linspace(0, 169, sol_overall[0].shape[0]), sol_overall[0])
+    plt.plot(np.linspace(0, 169, sol_overall[1].shape[0]), sol_overall[1])
+    plt.plot(np.linspace(0, 169, sol_overall[2].shape[0]), sol_overall[2])
+
+    plt.show()
--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
-from constants import alpha
+from dataclasses import dataclass
+from scipy.interpolate import interp1d
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from matplotlib import animation
 
 
 def isCorrectArray(a):
@@ -54,3 +59,131 @@ def tridiagonal_solution(a, b):
 
     return x
 
+
+def calculate_alpha(k, drho, mu, phi, denom=1000):
+    return k * drho * 9.81 / (mu * phi) / denom
+
+
+@dataclass
+class InputData:
+    path: str  # path to .dat file
+    cube_to_kg: float  # ratio from cubic meters of oil to kg
+    W: float # well drainage length
+    h_0: float  # height of GOC
+    beta: float  # ratio of pressures
+    phi: float  # coefficient of an effective porosity
+    zw1: float  # level of the well
+    dw: float  # diameter of the well
+    k: float  # permeability coefficient
+    drho: float  # difference of density
+    mu: float  # dinamic viscosity
+    Bg: float
+    Bo: float
+    gamma: float
+    L: float
+    p1: float
+    p2: float
+    n_x: int
+    n_t: int
+    zw2: float  #lower point of the well
+    t0: float
+    q0: float
+    p0: float
+    g: float = 9.80665
+    @property
+    def alpha(self):
+        return calculate_alpha(drho=self.drho, k=self.k, mu=self.mu, phi=self.phi)
+
+
+def diff_coef(z_nm, dt, dx, alpha):
+    return dt * alpha * (2 * z_nm) ** (1 / 2) / (dx ** 2)
+
+
+def well_flow_rate(y, q0, L, p1, p2):
+    assert 0 <= y <= L, 'Invalid y coordinate'
+    x0, y0, x1, y1 = 0, p1, L, p2
+    coef = (y1 - y0) / (x1 - x0)
+    bias = y1 - coef * x1
+    return q0 / L * (coef * y + bias)
+
+
+def solve(input_data: InputData):
+    with open(f'{input_data.path}', 'r') as f:
+        dat = f.read().split()
+
+    days = []
+    gas = []
+    oil = []
+    for i in range(len(dat) // 3):
+        days.append(float(dat[i * 3]))
+        oil.append(float(dat[i * 3 + 1]) * input_data.cube_to_kg)
+        gas.append(float(dat[i * 3 + 2]))
+
+    # interpolate oil
+    debit = interp1d(days, oil, fill_value='extrapolate')
+    n_x = input_data.n_x
+    n_t = input_data.n_t
+    dx = input_data.W / n_x
+    dt = int(max(days)) / input_data.t0
+    sol = np.zeros((n_t, n_x + 1))
+
+    # initial condition
+    sol[0] = np.array([input_data.h_0**2/2 for _ in range(n_x + 1)])
+    for t, curr_t in enumerate(tqdm(np.linspace(0, len(oil), n_t - 1))):
+        A = np.zeros((n_x + 1, n_x + 1))
+        b = np.zeros(n_x + 1)
+
+        # left boundary condition
+        y = input_data.L / 2
+
+        q_o = well_flow_rate(y, debit(curr_t), input_data.L, dt, dx) / (2 * input_data.alpha * input_data.phi)
+
+        b[0] = -sol[t][1] + diff_coef(sol[t][1], dt, dx, input_data.alpha) * (q_o * dx) / (
+                    input_data.alpha * input_data.phi)
+        A[0][1] = - (1 - 2 * diff_coef(sol[t][1], dt, dx, input_data.alpha))
+        A[0][0] = -2 * diff_coef(sol[t][1], dt, dx, input_data.alpha)
+
+        # finite diff scheme
+        for i in range(1, n_x):
+            b[i] = -sol[t][i]
+            A[i][i - 1] = diff_coef(sol[t][i], dt, dx, input_data.alpha)
+            A[i][i] = -1 - 2 * diff_coef(sol[t][i], dt, dx, input_data.alpha)
+            A[i][i + 1] = diff_coef(sol[t][i], dt, dx, input_data.alpha)
+
+        # right boundary condition
+        b[n_x] = -sol[t][n_x - 1]
+        A[n_x][n_x - 1] = -(1 - 2 * diff_coef(sol[t][n_x - 1], dt, dx, input_data.alpha))
+        A[n_x][n_x] = -2 * diff_coef(sol[t][n_x - 1], dt, dx, input_data.alpha)
+
+        x = tridiagonal_solution(A, b)
+
+        f = lambda v: input_data.zw2**2/2 if v < input_data.zw2**2/2 else v
+        x = np.vectorize(f)(x)
+
+        sol[t + 1] = x
+
+    return (2*sol)**(1/2)
+
+
+def animate(sol, input_data):
+    n_x = sol.shape[1]
+    n_t = 20
+
+
+    fig, ax = plt.subplots()
+
+    ax.axis([0, input_data.W, input_data.zw2-1, input_data.h_0])
+
+    l, = ax.plot([], [])
+
+    def anim_func(i):
+        l.set_data(np.linspace(0, input_data.W, n_x), sol[i*int(sol.shape[0]/n_t)])
+
+    ani = animation.FuncAnimation(fig, anim_func, frames=n_t)
+    plt.plot(np.linspace(0, input_data.W, n_x), [input_data.zw2 for _ in range(n_x)], label='Well bottom')
+    plt.plot(np.linspace(0, input_data.W, n_x), [input_data.zw1 for _ in range(n_x)], label='Well top')
+    plt.show()
+
+
+def plot_h0(sol):
+    plt.plot(np.linspace(0, 169, sol.shape[0]), sol[:, 0], label=f'{sol.shape[0]}, {sol.shape[1]}')
